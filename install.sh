aeb39f49a344de45073ec75b075ce524ddeea820
Akash Biswas INDIAN <96134576+akashbis2021@users.noreply.github.com>
2023-07-08 23:50:25 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 username=$(whoami)
-sudo git clone https://github.com/akashbis2021/Passkey_PasswordManager.git  /home/$username/Downloads
+mkdir /home/$username/Downloads/Passkey_PasswordManager
+sudo git clone https://github.com/akashbis2021/Passkey_PasswordManager.git  /home/$username/Downloads/Passkey_PasswordManager
 sudo mv /home/$username/Downloads/Passkey_PasswordManager/.passkey.sh  /var/log
 sudo chown -R $username:$username /var/log/.passkey.sh
 sudo chmod +x /var/log/.passkey.sh
